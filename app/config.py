c889de96a2431997ca83e9e70bf098f7cfe56a60
Hao Li <hli4@ebay.com>
2020-07-19 13:14:23 +0800

    db config diff with dev and prod

--- app/config.py
+++ app/config.py
@@ -14,15 +14,6 @@ class MyConfig(object):
     # SESSION_USE_SIGNER = True # 让 cookie 中的 session_id 被加密签名处理
     # PERMANENT_SESSION_LIFETIME = 86400 # session 的有效期，单位是秒
 
-    # 数据库的配置信息
-    DB_USER = "root"
-    DB_PASSWD = "password"
-    # defined host name in docker-compose, use docker's dns for connection.
-    DB_HOST = "mysql"
-    DB_PORT = 3306
-    DB_NAME = "flask_factory"
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{}:{}@{}:{}/{}".format(DB_USER, DB_PASSWD, DB_HOST, DB_PORT,
-                                                                             DB_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     def init_app(self):
@@ -32,10 +23,28 @@ class MyConfig(object):
 
 class ProductionConfig(MyConfig):
     DEBUG = False
+    # 数据库的配置信息
+    DB_USER = "root"
+    DB_PASSWD = "password"
+    # defined host name in docker-compose, use docker's dns for connection.
+    DB_HOST = "mysql"
+    DB_PORT = 3306
+    DB_NAME = "flask_factory"
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{}:{}@{}:{}/{}".format(DB_USER, DB_PASSWD, DB_HOST, DB_PORT,
+                                                                             DB_NAME)
 
 
 class DevelopmentConfig(MyConfig):
     DEBUG = True
+    # 数据库的配置信息
+    DB_USER = "root"
+    DB_PASSWD = "password"
+    # defined host name in docker-compose, use docker's dns for connection.
+    DB_HOST = "localhost"
+    DB_PORT = 3306
+    DB_NAME = "flask_factory"
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{}:{}@{}:{}/{}".format(DB_USER, DB_PASSWD, DB_HOST, DB_PORT,
+                                                                             DB_NAME)
 
 
 config_map = {
